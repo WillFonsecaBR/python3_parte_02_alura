aba53a406ecf0822c61e5aa54d9944e0371c8968
Willian Alves Fonseca <willian.alves2109@gmail.com>
2021-11-13 22:23:52 -0300

    AULA 5

---
+++ scripts/forca.py
@@ -0,0 +1,48 @@
+"""
+    JOGO DA FORCA EM PYTHON
+"""
+
+
+def jogar():
+    print("=============================")
+    print("====| BEM VINDO AO JOGO |====")
+    print("=============================")
+
+    palavra_secreta = "proparoxitona".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while not enforcou and not acertou:
+        chute = input("Qual é a letra? ")
+        chute = chute.strip().upper()
+
+        if chute in palavra_secreta:
+            index = 0
+
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        print("VOCÊ GANHOU")
+    else:
+        print("VOCÊ ERROU")
+
+    print("FIM DE JOGO")
+
+
+if(__name__ == "__main__"):
+    jogar()
